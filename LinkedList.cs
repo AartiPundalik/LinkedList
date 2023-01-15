eb47889dfe85b6e1034209be3295b10601d51165
AartiPundalik <aartipundalik68@gmail.com>
2023-01-15 17:44:00 +0530

    Welcome to the Link list

---
+++ LinkedList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        
+       
+            internal Node head;
+            internal void Addlast(int data)
+            {
+                Node newNode = new Node(data); 
+
+                if (this.head == null) 
+                {
+                    this.head = newNode;
+
+                }
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null) 
+                    {
+                        temp = (Node)temp.next;
+                    }
+                    temp.next = newNode;
+                }
+                Console.WriteLine("{0} is inserted in to Linked list", newNode.data);
+            }
+            public void Display()
+            {
+                Console.WriteLine("Display of Nodes");
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("LinkedList is Empty");
+                    return;
+                }
+                else
+                {
+                    while (temp != null)
+                    {
+                        Console.Write("" + temp.data + " ");
+                        temp = (Node)temp.next;
+
+                    }
+                }
+            }
+        }
+    }
+    
+
+
+
+
+
+
+
+    
+
---
+++ Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public  class Node
+    {
+        internal object next;
+        public int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+        public class Node1
+        {
+            public int data;
+            public Node next;
+            public Node1(int data)
+            {
+                this.data = data;
+            }
+        }
+    }
+  
+
+
+
+
+}
+
